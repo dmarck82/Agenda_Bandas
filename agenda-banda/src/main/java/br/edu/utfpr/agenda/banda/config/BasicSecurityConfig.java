84c927378c88c267500144c894d8dccd6f2c36f1
dmarck82 <douglas.marcondes@gmail.com>
2022-05-07 17:01:41 -0300

    Inicio implementação autenticação básica e oauth

---
+++ agenda-banda/src/main/java/br/edu/utfpr/agenda/banda/config/BasicSecurityConfig.java
@@ -0,0 +1,34 @@
+package br.edu.utfpr.agenda.banda.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Profile("basic-security")
+@Configuration
+@EnableWebMvcSecurity
+public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password("{noopadmin").roles("ROLE");
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .antMatchers("/banda").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .httpBasic()
+            .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .csrf().disable();
+    }
+}
